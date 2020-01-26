44a38d78eea772fd2df5b94107086aa0b7cf5784
kimserey <kimserey.lam@gmail.com>
2020-01-26 12:41:13 +0000

    Init

---
+++ test-xunit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_xunit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
